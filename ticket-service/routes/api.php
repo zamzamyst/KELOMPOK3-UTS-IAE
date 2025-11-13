6362f6bb31e1dc04ea3e5961c63a56dba6a470e8
Mufid Taqi <tmuhammadmufid@gmail.com>
2025-11-13 17:31:37 +0700

    update ticket

--- ticket-service/routes/api.php
+++ ticket-service/routes/api.php
@@ -6,5 +6,7 @@ use App\Http\Controllers\InternalController;
 Route::apiResource('tickets', TicketController::class);
 
 Route::prefix('internal')->group(function(){
+    Route::get('tickets/{id}', [InternalController::class,'show']);
     Route::put('tickets/{id}/status', [InternalController::class,'updateStatus']);
+    Route::put('tickets/{id}/amount', [InternalController::class,'updateAmount']);
 });
